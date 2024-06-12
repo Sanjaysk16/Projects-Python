d4caaf8d1d1975ad7544b61f553c3123a669cb5a
sanjaysk123s <sanjaysk0316@gmail.com>
2024-06-12 12:21:42 +0530

    Docstrings added

--- matrix.py
+++ matrix.py
@@ -1,4 +1,12 @@
 def get_matrices(rows: int, columns: int) -> list:
+    """
+    This function takes two integers, 'rows' and 'columns', as input and returns a list of lists, representing a matrix.
+    Once all the values are entered, the function returns the 'matrix' list.
+
+    :param rows: The number of rows in the matrix
+    :param columns: The number of columns in the matrix
+    :return: A list of lists, representing the matrix
+    """
     matrix = []
 
     for i in range(rows):
@@ -20,6 +28,14 @@ def flatten_matrix(mat):
 
 
 def add_matrices(matrices1, matrices2):
+    """
+    This function takes two matrices as input and returns a matrix.
+    Then, it iterates through each row and column, adding the corresponding elements of the two matrices.
+
+    :param matrices1: The first matrix
+    :param matrices2: The second matrix
+    :return: A matrix representing the sum of the two matrices
+    """
     row1 = len(matrices1)
     col1 = len(matrices1[0])  # len of the first row
     matrix = []
@@ -35,6 +51,15 @@ def add_matrices(matrices1, matrices2):
 
 
 def subtract_matrices(matrix1, matrix2):
+    """
+    This function takes two matrices as input and returns a matrix.
+    Finally, the function returns the resulting matrix.
+
+    :param matrix1: The first matrix
+    :param matrix2: The second matrix
+    :return: A matrix representing the difference of the two matrices
+    """
+
     row2 = len(matrix1)
     col2 = len(matrix1[0])
     matrix = []
@@ -47,6 +72,15 @@ def subtract_matrices(matrix1, matrix2):
 
 
 def multiply_matrix(matrix1, matrix2):
+    """
+    This function takes two matrices as input and returns a matrix.
+    Finally, the function returns the resulting matrix.
+
+    :param matrix1: The first matrix
+    :param matrix2: The second matrix
+    :return: A matrix representing the product of the two matrices
+    """
+
     matrix = []
     row3 = len(matrix1)
     col3 = len(matrix1[0])
@@ -61,6 +95,15 @@ def multiply_matrix(matrix1, matrix2):
 
 
 def division_matrix(mat1: list, mat2: list):
+    """
+    This function takes two matrices as input and returns a matrix.
+    If the second matrix is singular, it raises an error.
+    Finally, the function returns the resulting matrix.
+
+    :param mat1: The first matrix
+    :param mat2: The second matrix
+    :return: A matrix representing the quotient of the two matrices
+    """
     row = len(mat1)
     col = len(mat1[0])
     result = []
